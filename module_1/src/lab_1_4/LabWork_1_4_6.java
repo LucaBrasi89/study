eea233e27f5cb7d0da728aa06d972aaaf6b02e99
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-01-19 20:47:53 +0200

    Доблавен git-ignore

---
+++ module_1/src/lab_1_4/LabWork_1_4_6.java
@@ -0,0 +1,31 @@
+package lab_1_4;
+
+/**
+ * Created by andrew on 19.01.17.
+ *  TASK:
+ *
+ *  Write a console program that prints result of Bitwise operators for two
+ *  integer variables.
+ *
+ */
+public class LabWork_1_4_6 {
+
+    public static void main(String[] args) {
+
+        int iVal1 = 0b1000_00;
+        int iVal2 = 0b1010_10;
+
+        System.out.println("iVal1 = "+10_0000);
+        System.out.println("iVal2 = "+10_1010);
+
+        System.out.println("iVal1 & iVal2  =  " + (iVal1 & iVal2));
+        System.out.println("iVal1 | iVal2  =  " + (iVal1 | iVal2));
+        System.out.println("iVal1 ^ iVal2  =  " + (iVal1 ^ iVal2));
+        System.out.println("~iVal1 =  " + (~iVal1));
+
+
+
+    }
+
+
+}
