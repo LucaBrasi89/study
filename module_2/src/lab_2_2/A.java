2a01c6c2e305dc68e34187ad65bd676e381fe4ad
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-02-03 22:49:19 +0200

    Вычисление площадей фигур. Сделано на 1/2

---
+++ module_2/src/lab_2_2/A.java
@@ -0,0 +1,50 @@
+package module_2.src.lab_2_2;
+
+/**
+ * Created by andrew on 02.02.17.
+ */
+/*
+
+        =====Laboratory work 2.2.5=====
+
+    Создать класс с методами для вычисления площади круга, прямоугольника,
+    квадрата.
+
+*/
+
+
+
+public class A {
+
+    //    прямоугольник
+    public int calcSquare (int rectangeleSide1, int rectangeleSide2) {
+
+        int square = rectangeleSide1 * rectangeleSide2;
+        System.out.println("Rectangle square is: " + square);
+
+        return square;
+
+    }
+
+    //    квадрат
+    public int calcSquare (int forsquareSide) {
+
+        int square = forsquareSide ^ 2;
+        System.out.println("Forsquare square is: " + square);
+
+        return square;
+
+    }
+
+    //    окружность
+    public double calcSquare(double diametre) {
+
+        double square = Math.PI * ((diametre*diametre)/4);
+        System.out.println("Circle square is: " + square);
+
+        return square;
+
+    }
+
+
+}
---
+++ module_2/src/lab_2_2/UseSquares.java
@@ -0,0 +1,31 @@
+package module_2.src.lab_2_2;
+
+/**
+ * Created by andrew on 02.02.17.
+ */
+
+/*
+
+        =====Laboratory work 2.2.5=====
+
+    Подсчитать площади фигур.
+
+*/
+
+public class UseSquares {
+
+    public static void main(String[] args) {
+
+        A figure = new A();
+
+        figure.calcSquare(11,20);
+        figure.calcSquare(40);
+        figure.calcSquare(121.1);
+
+
+
+    }
+
+
+
+}
