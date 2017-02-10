6542e2757fa1acfb8d610a1b3bee1ce7be68c035
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-02-10 22:07:11 +0200

    свежие jar'ники

---
+++ module_2/src/lab_2_6/AnotherCar.java
@@ -0,0 +1,15 @@
+package module_2.src.lab_2_6;
+import example.testpack.Main;
+
+/**
+ * Created by andrew on 10.02.17.
+ */
+public class AnotherCar {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
