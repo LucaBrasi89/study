1722733f3d6c63bc9e7733251c9c82155a042511
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-03-04 22:32:07 +0200

    Unique chars in array

--- module_2/src/lab_2_9/TestStrings3.java
+++ module_2/src/lab_2_9/TestStrings3.java
@@ -1,35 +1,63 @@
 package lab_2_9;
 
+import example.testpack.Main;
+
 import java.util.Arrays;
 
 /**
  * Created by andrew on 04.03.17.
+
+             ============ Lab work 2.9.3 ============
+ Create static method: uniqueChars(String s), which must take a String as argument and
+ return an array of distinct (unique) chars (char[]) of the given string.
+ In method main() add code to invoke uniqueChars() method and pass string: “Using
+ methods of class String” as argument. Print result to console.
+ Execute the program.
  */
+
 public class TestStrings3 {
 
-    static String uniqueChars(String str){
 
-        char[] uniqueChars = new char[0];
-        for (int i = 0; i < str.length() ; i++) {
+    private static char[] uniqueChars; // массив уникальных символов
 
-            int count = 0;
+    public static void uniqueChars(String str) {
+
+        uniqueChars = new char[0];
+        for (int i = 0; i < str.length(); i++) {
             char curChar = str.charAt(i);
-            if ( str.indexOf() > 1 ) {
-
-                uniqueChars = Arrays.copyOf(uniqueChars, uniqueChars.length
-                        +1);
-                uniqueChars[uniqueChars.length -1]  = curChar;
-
+//            если массив пустой молча добавим туда пустой символ. Иначе схватим error
+            if (uniqueChars.length == 0) {
+                uniqueChars = Arrays.copyOf(uniqueChars, uniqueChars.length + 1);
+                uniqueChars[uniqueChars.length - 1] = curChar;
+                continue;
             }
-
+            if (checkUniqueValue(curChar) == true) {
+                uniqueChars = Arrays.copyOf(uniqueChars, uniqueChars.length + 1);
+                uniqueChars[uniqueChars.length - 1] = curChar;
+            }
         }
-
-
     }
+
+//    проверяет нету ли такоего символа в массиве uniqueChars
+    public static boolean checkUniqueValue(char src) {
+
+        boolean unique = false;
+        for (char symb : uniqueChars) {
+            if (symb != src) {
+                unique = true;
+            }
+            else {
+                return false;
+            }
+        }
+        return unique;
+    }
+
 
     public static void main(String[] args) {
 
-
+        TestStrings3.uniqueChars("Using methods of class String");
+        System.out.println(TestStrings3.uniqueChars);
     }
 
 }
