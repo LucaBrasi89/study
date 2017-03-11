5e1981180b85510d885dafcdc350431c6a28d3f7
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-03-11 19:46:49 +0200

    Exceptions

---
+++ module_2/src/lab_2_11/Main.java
@@ -0,0 +1,20 @@
+package lab_2_11;
+
+/**
+ * Created by andrew on 11.03.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            throw new Exception("something occured");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+
+            System.out.println("I was here... In finally block");
+        }
+    }
+
+}
