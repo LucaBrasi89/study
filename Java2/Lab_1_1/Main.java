d76650588e9801b7d4bc155b6750daa6dfd74a1b
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-04-22 20:46:57 +0300

    java2

---
+++ Java2/Lab_1_1/Main.java
@@ -0,0 +1,44 @@
+package Lab_1_1;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by andrew on 22.04.17.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        String source = "/media/Maindata/Дело/study/Java " +
+                "Exersises/Java2/Lab_1_1/PackageToCopy/";
+        String destination = "/media/Maindata/Дело/study/Java " +
+                "Exersises/Java2/Lab_1_1/PackageCopy/";
+
+
+
+
+        File file = new File("/media/Maindata/Дело/study/Java Exersises/Java2/Lab_1_1/PackageToCopy/");
+
+
+        for (String fileName : file.list()) {
+
+            InputStream istr = new FileInputStream(source + fileName) ;
+            OutputStream ostr = new FileOutputStream(destination + fileName);
+
+            try {
+                Files.copy(Paths.get(destination+fileName), ostr);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        FileOutputStream output = new FileOutputStream(Files.walk(Paths.get
+                (source));
+
+
+    }
+
+
+}
