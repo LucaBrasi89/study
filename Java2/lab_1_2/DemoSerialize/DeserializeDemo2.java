3a3f5906c58f462816070fe6474f9d8fa0aa56f9
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-04-29 08:54:16 +0300

    Task 1.2.5

---
+++ Java2/lab_1_2/DemoSerialize/DeserializeDemo2.java
@@ -0,0 +1,35 @@
+package lab_1_2.DemoSerialize;
+
+import java.io.*;
+
+/**
+ * Created by andrew on 23.04.17.
+ *
+ *                            Lab Work 1-2-5
+ *
+         1) Modify class User replacing the implementation of Serializable on interface
+         Externalizable. Override methods writeExternal (ObjectOutput out) and readExternal
+         (Object Input in);
+         2) Create a class Main with a main() method, which initializes an array of User users;
+         3) Add code in a main() method that writes an array of users in the "userfile.ser" file;
+         4) Add code in a main() method that reads from a file "userfile.ser" all users and prints
+         them to the console.
+         5) Execute the program.
+ *
+ */
+public class DeserializeDemo2 {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Employee2[] users = {new Employee2("Andrew", "Kyiv", 123, 051),
+                new Employee2("Kate", "Kyiv", 124, 052),
+                new Employee2("Ann", "Kyiv", 125, 053)};
+
+
+        for (Employee2 user : users) {
+            user.writeExternal((ObjectOutput) user);
+        }
+
+    }
+
+}
---
+++ Java2/lab_1_2/DemoSerialize/Employee2.java
@@ -0,0 +1,98 @@
+package lab_1_2.DemoSerialize;
+
+import java.io.*;
+
+/**
+ * Created by andrew on 23.04.17.
+
+                                Lab Work 1-2-5
+
+         1) Modify class User replacing the implementation of Serializable on interface
+         Externalizable. Override methods writeExternal (ObjectOutput out) and readExternal
+         (Object Input in);
+         2) Create a class Main with a main() method, which initializes an array of User users;
+         3) Add code in a main() method that writes an array of users in the "userfile.ser" file;
+         4) Add code in a main() method that reads from a file "userfile.ser" all users and prints
+         them to the console.
+         5) Execute the program.
+
+ */
+public class Employee2 implements Externalizable {
+
+    private String name;
+    private String addressl;
+    private transient int SSN;
+    private int number;
+
+    public Employee2(String name, String addressl, int SSN, int number) {
+        this.name = name;
+        this.addressl = addressl;
+        this.SSN = SSN;
+        this.number = number;
+    }
+
+    public Employee2() {
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressl() {
+        return addressl;
+    }
+
+    public void setAddressl(String addressl) {
+        this.addressl = addressl;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", addressl='" + addressl + '\'' +
+                ", SSN=" + SSN +
+                ", number=" + number +
+                '}';
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+
+        FileOutputStream fis = new FileOutputStream("Java2/lab_1_2/DemoSerialize/users.ser");
+        out.writeObject(name);
+        out.writeObject(addressl);
+        out.writeObject(SSN);
+        out.writeObject(number);
+        fis.flush();
+        fis.close();
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+    }
+}
