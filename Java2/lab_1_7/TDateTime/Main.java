39563407336586e25979749477577351da07f1af
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-05-06 23:04:28 +0300

    date and time

--- Java2/lab_1_7/TDateTime/Main.java
+++ Java2/lab_1_7/TDateTime/Main.java
@@ -1,7 +1,6 @@
 package lab_1_7.TDateTime;
 
-import java.time.LocalDate;
-import java.time.Period;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -18,20 +17,31 @@ public class Main {
         System.out.println(curDate.until(birthDay));
 
         Period per = Period.between(birthDay, curDate);
-        System.out.println("I am " + per.getYears()+ " years old");
+        System.out.println("I am " + per.getYears() + " years old");
         System.out.println("This is day of week of my birthday in 89th: " +
                 birthDay.getDayOfWeek());
         LocalDate thisYearbithday = birthDay.plusYears(28);
         System.out.println("This is day of week of my birthday in this year: " +
                 thisYearbithday.getDayOfWeek());
 
-        Period betweenMyBirthday = Period.between(curDate,thisYearbithday);
+        Period betweenMyBirthday = Period.between(curDate, thisYearbithday);
 //        check on birthDay in this year, was or wasn\'t'
         if (betweenMyBirthday.isNegative()) {
             System.out.println("birth passed");
 
         }
 
-    }
 
+//        choosing of timezones, specify formats and output it
+        System.out.println();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy, HH-mm-ss");
+
+        LocalDateTime tokyo = LocalDateTime.ofInstant(Instant.now(), ZoneId.of("Asia/Tokyo"));
+        LocalDateTime losAngeles = LocalDateTime.ofInstant(Instant.now(), ZoneId.of("America/Los_Angeles"));
+        LocalDateTime helsinki = LocalDateTime.ofInstant(Instant.now(), ZoneId.of("Europe/Helsinki"));
+
+        System.out.println("Tokyo " + formatter.format(tokyo));
+        System.out.println("Los Angeles " + formatter.format(losAngeles));
+        System.out.println("Helsinki " + formatter.format(helsinki));
+    }
 }
