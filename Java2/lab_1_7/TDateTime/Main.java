d82d83199a4a46e5e86616fd1f20842df90d9f3b
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2017-05-16 22:48:45 +0300

    date and time

--- Java2/lab_1_7/TDateTime/Main.java
+++ Java2/lab_1_7/TDateTime/Main.java
@@ -7,7 +7,6 @@ import java.time.format.DateTimeFormatter;
  * Created by andrew on 06.05.17.
  */
 public class Main {
-
     public static void main(String[] args) {
 
         LocalDate curDate = LocalDate.now();
