05f340186cea4173828c856d8f54edde12307753
Andrew <andrew.sotnikov@zoho.com>
2017-06-14 23:55:56 +0300

    main window without a table

--- SunAirport/core/src/UI/Main.java
+++ SunAirport/core/src/UI/Main.java
@@ -38,7 +38,7 @@ public class Main {
         c.gridx = 0;
         c.gridy = 0;
         c.gridwidth = 2;
-
+        c.insets = new Insets(10,0,10,0);
         JLabel invitationText = new JLabel("Welcome to Sunrise Airport!", SwingConstants.CENTER);
 
         panel.add(invitationText, c);
@@ -47,13 +47,11 @@ public class Main {
 
     public void doTable() {
 
-
-//        c.fill = GridBagConstraints.HORIZONTAL;
 //        do a tabbed panel
         c.gridx = 1;
         c.gridy = 1;
-        c.ipadx = 600;
-        c.ipady = 600;
+        c.ipadx = 520;
+        c.ipady = 650;
 
         JTabbedPane tabbedPane = new JTabbedPane();
         JComponent panel1 = new JPanel();
@@ -64,6 +62,7 @@ public class Main {
         tabbedPane.addTab("                 Depature              ", panel2);
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
 //        tabbedPane.setSize(900,600);
+
 
 
         panel.add(tabbedPane, c);
@@ -77,41 +76,54 @@ public class Main {
         c.gridx = 0;
         c.gridy = 1;
         c.gridwidth = 1;
+        c.insets = new Insets(0,10,0,10);
 
 //        do a left sidebar
-//        c.weightx = 0.1;
+
 
 
         JPanel sideBar = new JPanel(new GridBagLayout());
+//        sideBar.setMinimumSize(new Dimension(200,80));
         GridBagConstraints gbc = new GridBagConstraints();
+
         gbc.fill = GridBagConstraints.HORIZONTAL;
         JLabel title = new JLabel("Please, login");
         gbc.gridx = 0;
         gbc.gridy = 0;
+        gbc.gridwidth = 2;
 
+        gbc.insets = new Insets(0,0,10, 0);
 
         sideBar.add(title,gbc);
 
         JLabel label1 = new JLabel("Login:");
         gbc.gridx = 0;
         gbc.gridy = 1;
+        gbc.ipadx = 30;
+        gbc.gridwidth = 1;
+
+        label1.setMinimumSize(new Dimension(60,20));
         sideBar.add(label1, gbc);
 
         JTextField loginField = new JTextField();
         gbc.gridx = 1;
         gbc.gridy = 1;
-        gbc.ipadx = 120;
+        gbc.ipadx = 100;
+
+
         sideBar.add(loginField,gbc);
 
         JLabel label2 = new JLabel("Password:");
         gbc.gridx = 0;
         gbc.gridy = 2;
+        gbc.ipadx = 30;
         sideBar.add(label2,gbc);
 
         JPasswordField passwordField = new JPasswordField(10);
         gbc.gridx = 1;
         gbc.gridy = 2;
-        gbc.ipadx = 120;
+        gbc.ipadx = 100;
+
         sideBar.add(passwordField,gbc);
 
 
@@ -130,8 +142,5 @@ public class Main {
 
         window.frame.validate();
     }
-
-
-
 
 }
