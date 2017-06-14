fcde83b82d016a90a1cf6b3cab2fb0b1e346d424
Andrew <andrew.sotnikov@zoho.com>
2017-06-14 22:35:50 +0300

    project step

--- SunAirport/core/src/UI/Main.java
+++ SunAirport/core/src/UI/Main.java
@@ -1,6 +1,7 @@
 package UI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 
 /**
@@ -9,9 +10,8 @@ import java.awt.event.KeyEvent;
 public class Main {
 
     private JFrame frame;
-
-
-
+    private GridBagConstraints c;
+    private  JPanel panel;
 
     public void doMainWindow() {
 
@@ -20,18 +20,103 @@ public class Main {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JTabbedPane tabbedPane = new JTabbedPane();
-        frame.add(tabbedPane);
 
+        panel = new JPanel(new GridBagLayout());
+        panel.setSize(1000,700);
+        frame.add(panel);
+
+        frame.setVisible(true);
+
+
+    }
+
+    public void doHeader() {
+
+        c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 2;
+
+        JLabel invitationText = new JLabel("Welcome to Sunrise Airport!", SwingConstants.CENTER);
+
+        panel.add(invitationText, c);
+
+    }
+
+    public void doTable() {
+
+
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        do a tabbed panel
+        c.gridx = 1;
+        c.gridy = 1;
+        c.ipadx = 600;
+        c.ipady = 600;
+
+        JTabbedPane tabbedPane = new JTabbedPane();
         JComponent panel1 = new JPanel();
-        tabbedPane.addTab("Arrival", panel1);
+        tabbedPane.addTab("                Arrival                ", panel1);
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
         JComponent panel2 = new JPanel();
-        tabbedPane.addTab("Depature", panel2);
+        tabbedPane.addTab("                 Depature              ", panel2);
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+//        tabbedPane.setSize(900,600);
 
-        frame.setVisible(true);
+
+        panel.add(tabbedPane, c);
+
+
+    }
+
+    public void doSidebar() {
+
+//        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 1;
+
+//        do a left sidebar
+//        c.weightx = 0.1;
+
+
+        JPanel sideBar = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        JLabel title = new JLabel("Please, login");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+
+        sideBar.add(title,gbc);
+
+        JLabel label1 = new JLabel("Login:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        sideBar.add(label1, gbc);
+
+        JTextField loginField = new JTextField();
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.ipadx = 120;
+        sideBar.add(loginField,gbc);
+
+        JLabel label2 = new JLabel("Password:");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        sideBar.add(label2,gbc);
+
+        JPasswordField passwordField = new JPasswordField(10);
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.ipadx = 120;
+        sideBar.add(passwordField,gbc);
+
+
+        panel.add(sideBar, c);
+
 
     }
 
@@ -39,7 +124,11 @@ public class Main {
 
         Main window = new Main();
         window.doMainWindow();
+        window.doHeader();
+        window.doSidebar();
+        window.doTable();
 
+        window.frame.validate();
     }
 
 
