78a66a5ee55d81b0729852adab146916c844b4a5
Andrew <andrew.sotnikov@zoho.com>
2017-06-22 22:10:10 +0300

    almoust complete GUI

---
+++ SunriseAirport/core/src/UI/App.java
@@ -0,0 +1,32 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by andrew on 22.06.17.
+ */
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+
+        launch();
+    }
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Sunrise");
+        primaryStage.show();
+
+
+    }
+}
