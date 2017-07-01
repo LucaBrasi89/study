9b08be4231d375563f9cf774f56ab9cc84d9d71d
Andrew <andrew.sotnikov@zoho.com>
2017-07-01 16:24:15 +0300

    sidebar working

--- SunriseAirport/core/src/UI/MainController.java
+++ SunriseAirport/core/src/UI/MainController.java
@@ -4,10 +4,11 @@ import BL.Flight;
 import DL.FetchFlights;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,6 +28,15 @@ public class MainController implements Initializable {
 
     @FXML
     private MenuBar rootMenu;
+
+    @FXML
+    private TextField loginInput;
+
+    @FXML
+    private PasswordField passwdInput;
+
+    @FXML
+    private VBox sidebar;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -61,18 +71,47 @@ public class MainController implements Initializable {
         depatureTable.setItems(flights);
     }
 
-    public void hideMenu() {
 
-        rootMenu.setVisible(false);
+
+    public void chownRoot(ActionEvent actionEvent) {
+
+//        account passed
+        if (ifRoot(loginInput.getText(), passwdInput.getText())) {
+
+//          showing meny and hide sidebar
+            rootMenu.setVisible(true);
+            sidebar.setVisible(false);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Hello!");
+            alert.setHeaderText("Perfect");
+            alert.setContentText("You have logged like personal stuff");
+            alert.show();
+
+//            account failed
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Try again...");
+            alert.setHeaderText("We are sorry...");
+            alert.setContentText("but you are trying to type wrong login or password.");
+            alert.show();
+
+        }
 
     }
 
-    public void showMenu() {
 
-        rootMenu.setVisible(true);
+    public boolean ifRoot(String user, String passwd) {
+
+        String originUser = "root" ;
+        String originPasswd = "root";
+        if (originUser.equals(user) && originPasswd.equals(passwd)) {
+
+            return true;
+        } else {
+
+            return false;
+        }
     }
-
-
-
 }
 
